d73d5e2edea010ff6f3759cce0e62d664899f946
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-07 13:34:50 -0300

    AC-LP BDD INW

--- pethouse.js
+++ pethouse.js
@@ -15,15 +15,15 @@ mongoose.connect("mongodb://127.0.0.1:27017/pethouse", {
 })
 
 //models
-const UserSchema = new mongoose.Schema({
+const UsuarioSchema = new mongoose.Schema({
     email: {type: String, required: true},
-    password: {type: String}
+    senha: {type: String}
 })
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UsuarioSchema);
 
 const produtopet = new mongoose.Schema({
     id: {type: String, required: true},
-    desc: {type: String},
+    descricao: {type: String},
     fornecedor: {type: String},
     dataValidade: {type: Date},
     estoque: {type: Number}
@@ -33,14 +33,14 @@ const produto = mongoose.model('produto', produtopet);
 //post
 app.post("/cadastrousuario", async (req, res)=>{
     const email = req.body.email;
-    const password = req.body.password;
+    const senha = req.body.senha;
 
-    if(email == null || password == null){
+    if(email == null || senha == null){
         return res.status(400).json({error:"Preencha todos os campos"});
     }
     const user = new User({
         email: email,
-        password: password
+        senha: senha
     })
     try{
     const newUser = await user.save();
@@ -49,21 +49,26 @@ app.post("/cadastrousuario", async (req, res)=>{
     catch(error){
         res.status.json({error});
     }
+    const emailExiste = await Pessoa.findOne({email: email})
+
+    if(emailExiste){
+        return res.status(400).json({error: "O email cadastrado já existe"})
+    }
 })
 
 app.post("/cadastroProdutoPet", async (req, res)=>{
     const id = req.body.id;
-    const desc = req.body.desc;
+    const descricao = req.body.descricao;
     const fornecedor = req.body.fornecedor;
     const dataValidade = req.body.dataValidade;
     const estoque = req.body.estoque;
 
-    if(id == null || desc == null || fornecedor == null || dataValidade == null || estoque == null){
+    if(id == null || descricao == null || fornecedor == null || dataValidade == null || estoque == null){
         return res.status(400).json({error:"Preencha todos os campos"});
     }
     const produtopet = new produto({
         id: id,
-        desc: desc,
+        descricao: descricao,
         fornecedor: fornecedor,
         dataValidade: dataValidade,
         estoque: estoque
@@ -74,6 +79,12 @@ app.post("/cadastroProdutoPet", async (req, res)=>{
     }
     catch(error){
         res.status.json({error});
+    }
+    if(produto>45){
+        res.status(400).json({error:"O limite do produto foi atingido"})
+    }
+    else if(0<=produto){
+        res.status(400).json({error:"O produto predcisa te no mínimo 1 unidade"})
     }
 })
 
